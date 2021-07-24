3ae1facec07a73bcbc866dd31338ff24720f1cca
irisTa56 <takayuki.kobayashi@access-company.com>
2021-07-24 15:27:42 +0900

    fix indent

--- contract/contracts/Ballot.sol
+++ contract/contracts/Ballot.sol
@@ -2,38 +2,33 @@
 pragma solidity ^0.8.0;
 
 contract Ballot {
-  struct Voter {
-    uint weight;
-    bool voted;
-  }
+    struct Voter {
+        uint weight;
+        bool voted;
+    }
 
-  address public chairperson;
-  mapping (address => Voter) voters;
-  mapping (uint => uint) proposals;
+    address public chairperson;
+    mapping (address => Voter) voters;
+    mapping (uint => uint) proposals;
 
-  modifier onlyChair() {
-    require(
-      msg.sender == chairperson
-    );
-    _;
-  }
+    modifier onlyChair() {
+        require(msg.sender == chairperson, "onlyChair");
+        _;
+    }
 
-  modifier validVoter() {
-    require(
-      voters[msg.sender].weight > 0,
-      "Not a Registered Voter"
-    );
-    _;
-  }
+    modifier validVoter() {
+        require(voters[msg.sender].weight > 0, "notRegisteredVoter");
+        _;
+    }
 
-  constructor () {
-    chairperson = msg.sender;
-    voters[chairperson].weight = 2;
-  }
+    constructor () {
+        chairperson = msg.sender;
+        voters[chairperson].weight = 2;
+    }
 
-  function register(address voter) public onlyChair {}
+    function register(address voter) public onlyChair {}
 
-  function vote(uint toProposal) public validVoter {}
+    function vote(uint toProposal) public validVoter {}
 
-  function reqWinner() public view returns (uint) {}
+    function reqWinner() public view returns (uint) {}
 }

9db54112157578666130a2efaddbf0c4c7edb1e6
irisTa56 <takayuki.kobayashi@access-company.com>
2021-07-24 13:28:00 +0900

    change path to phoenix welcome page

--- app/lib/hobby_dapp_web/router.ex
+++ app/lib/hobby_dapp_web/router.ex
@@ -13,30 +13,27 @@ defmodule HobbyDappWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", HobbyDappWeb do
+  scope "/phoenix", HobbyDappWeb do
     pipe_through :browser
 
     get "/", PageController, :index
+
+    # Enables LiveDashboard only for development
+    #
+    # If you want to use the LiveDashboard in production, you should put
+    # it behind authentication and allow only admins to access it.
+    # If your application does not have an admins-only section yet,
+    # you can use Plug.BasicAuth to set up some basic authentication
+    # as long as you are also using SSL (which you should anyway).
+    if Mix.env() in [:dev, :test] do
+      import Phoenix.LiveDashboard.Router
+
+      live_dashboard "/dashboard", metrics: HobbyDappWeb.Telemetry
+    end
   end
 
   # Other scopes may use custom stacks.
   # scope "/api", HobbyDappWeb do
   #   pipe_through :api
   # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: HobbyDappWeb.Telemetry
-    end
-  end
 end
--- app/test/hobby_dapp_web/controllers/page_controller_test.exs
+++ app/test/hobby_dapp_web/controllers/page_controller_test.exs
@@ -2,7 +2,7 @@ defmodule HobbyDappWeb.PageControllerTest do
   use HobbyDappWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
+    conn = get(conn, "/phoenix")
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
 end
